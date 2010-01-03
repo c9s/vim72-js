85ee63c57de138b2b03913d3be895dae407592cf
c9s <cornelius.howl@gmail.com>
2010-01-03 11:05:31 +0800

    javascript api test code.

--- src/test.js
+++ src/test.js
@@ -1,12 +1,44 @@
 
 //system('ls');
-message('orz');   // show vim message
-alert('orz');   // show vim message  (alias to message function)
+//message('orz');   // show vim message
+//alert('orz');   // show vim message  (alias to message function)
 
-var v = buf_cnt();
-message( v );
+function ok(v,comment) {
+    if( v ) 
+	message( "ok - " + comment );
+    else
+	message( "fail - " + comment );
+}
+
+/* 
+function is(v,t,comment) {
+    //arguments.callee.caller.toString()
+    if( v == t ) 
+	message( "ok - " + comment );
+    else
+	message( "fail - " + comment );
+}
+
+*/
+
+var i = buf_cnt();
+message( i );
+ok( i , 'buf_cnt' );
+ok( buf_cnt() , 'buf_cnt');
+
+var b = buf_nr(1);
+ok( b , 'buf_nr' );
+ok( buf_ffname(b) );
+
+message( buf_number( b ) );
+
+//is( buf_ffname(b) , '/Users/c9s/git-working/vim7/src/test.js' , 'buf_ffname: ' + buf_ffname(b)  );
 
 
-var b = buf_nr(0);
+/* 
+if( name ) 
+    message( name );
+else
+    message( 'buf_ffname not ok' );
 
-
+    */
